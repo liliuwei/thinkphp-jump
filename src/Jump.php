9b60d79cf15f8b0a203f47daf1a1b8586bdeab5c
liliuwei <974829947@qq.com>
2019-12-04 14:20:41 +0800

    增强单独调用

--- src/Jump.php
+++ src/Jump.php
@@ -8,14 +8,40 @@
 
 namespace liliuwei\think;
 use think\exception\HttpResponseException;
+use think\App;
 use think\Response;
 trait Jump
 {
+    /**
+     * Request实例
+     * @var \think\Request
+     */
+    protected $request;
+
     /**
      * 应用实例
      * @var \think\App
      */
     protected $app;
+
+    /**
+     * 构造方法
+     * @access public
+     * @param  App  $app  应用对象
+     */
+    public function __construct(App $app)
+    {
+        $this->app     = $app;
+        $this->request = $this->app->request;
+
+        // 控制器初始化
+        $this->initialize();
+    }
+
+    // 初始化
+    protected function initialize()
+    {}
+
     /**
      * 操作成功跳转的快捷方法
      * @access protected
